18073f0db859bfd2f4d650ffe77dcc5feab03e26
Muhammad-Munsif <m.munsif4375@gmail.com>
2026-05-05 22:53:15 +0500

    this is a small changes inside the css style to make it great

--- nexus-backend/server.js
+++ nexus-backend/server.js
@@ -9,8 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
 app.use('/api/auth', authRoutes);
 
