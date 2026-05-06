96464bc60c4955d5194d0b1e7d36e53ea805755f
Muhammad-Munsif <m.munsif4375@gmail.com>
2026-05-06 23:51:13 +0500

    this is a small changes are in css and js file

--- nexus-backend/server.js
+++ nexus-backend/server.js
@@ -1,23 +1,23 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const authRoutes = require('./routes/auth');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-app.use('/api/auth', authRoutes);
-app.get('/', (req, res) => res.send('Nexus API is running'));
+app.use("/api/auth", authRoutes);
+app.get("/", (req, res) => res.send("Nexus API is running"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
-
 
 // require('dotenv').config();
 // const express = require('express');
